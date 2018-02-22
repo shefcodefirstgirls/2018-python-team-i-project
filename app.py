eff5364026794ce7cf66436dd0fe2d8ad6347827
Darren Vong <d.vongy@gmail.com>
2018-02-22 08:37:40 +0000

    Extended basic app to handle POST requests

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("my_first_app")
 
 @app.route("/")
 def say_hello():
-    return "hello world!"
+    return render_template("index.html")
 
-app.run(debug=True, host="0.0.0.0")
+@app.route("/<name>")
+def say_hello_to(name):
+    return render_template("hello.html", user=name)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+    data = request.values
+
+    return render_template("feedback.html", form_data=data)
+
+app.run(debug=True)
